c290bf77d7ce2c138e05373346e01c6e2491405a
Adam Heins <mail@adamheins.com>
2021-08-11 22:37:59 -0400

    Remove macros for now.

--- include/CppADCodeGenEigenPy/ADMacro.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define BEGIN_AD_MODEL(Model, Scalar)              \
-    struct Model : ADModel<Scalar> {               \
-        Model(size_t input_dim, size_t output_dim) \
-            : ADModel("Model", input_dim, output_dim){};
-
-#define END_AD_MODEL };
