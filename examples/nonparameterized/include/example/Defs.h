2c0e83155d550451fb28f498784661c2c6c0b7e7
Adam Heins <mail@adamheins.com>
2021-10-26 17:39:51 -0400

    Remove examples.

--- examples/nonparameterized/include/example/Defs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-static const std::string MODEL_NAME = "MyModel";
-static const std::string FOLDER_NAME = "lib";
-static const std::string LIB_NAME = FOLDER_NAME + "/lib" + MODEL_NAME;
-
-static const int NUM_INPUT = 3;
